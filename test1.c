7d55dabad19d04fe83cc1d4542e57d3039cfb9f0
Sofus Jejlskov Brandt <s214972@dtu.dk>
2024-11-19 10:57:49 +0100

    actually made it test for waiting lol

--- test1.c
+++ test1.c
@@ -24,29 +24,21 @@
 static AlarmQueue q;
 
 void * producer (void * arg) {
+    printf("Sending alarm 1...\n");
     put_alarm(q, 1);
-    msleep(20);
+    printf("Sending alarm 2...\n");
     put_alarm(q, 2);
-    msleep(100);
-    put_alarm(q, 3);
 
     return 0;
 }
 
 void * consumer(void * arg) {
-    msleep(50);
+    msleep(1000);
     get(q);
+    msleep(20);
     get(q);
 
     return 0;
-}
-
-void * qobserver(void * arg) {
-  //msleep(500);
-  print_sizes(q);
-  print_sizes(q);
-  print_sizes(q);
-  return 0;
 }
 
 int main(int argc, char ** argv) {
@@ -61,27 +53,23 @@ int main(int argc, char ** argv) {
 
     pthread_t t1;
     pthread_t t2;
-    pthread_t t3;
 
     void * res1;
     void * res2;
-    void * res3;
 
     printf("----------------\n");
 
     /* Fork threads */
     pthread_create(&t1, NULL, producer, NULL);
     pthread_create(&t2, NULL, consumer, NULL);
-    pthread_create(&t3, NULL, qobserver, NULL);
 
     /* Join with all threads */
     pthread_join(t1, &res1);
     pthread_join(t2, &res2);
-    pthread_join(t3, &res3);
 
 
     printf("----------------\n");
-    printf("Threads terminated with %ld, %ld, %ld\n", (uintptr_t) res1, (uintptr_t) res2, (uintptr_t) res3);
+    printf("Threads terminated with %ld, %ld\n", (uintptr_t) res1, (uintptr_t) res2);
 
     print_sizes(q);
 
